20aebcfe148a69abf4e1608b9714f79b81d823cb
Kalix Zee <zeekalix@gmai.com>
2019-01-22 16:26:29 -0800

    Updates

---
+++ precache-manifest.48962bce8ec0fdf3189023d1f5e00b8f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "39ff8e544d138b59deec",
+    "url": "/playground/static/css/main.08342e37.chunk.css"
+  },
+  {
+    "revision": "39ff8e544d138b59deec",
+    "url": "/playground/static/js/main.39ff8e54.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/playground/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "fd2222a613df36f2a7bd",
+    "url": "/playground/static/js/runtime~main.fd2222a6.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/playground/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "1570c33209d2a77c95199b04a7778aed",
+    "url": "/playground/index.html"
+  }
+];
