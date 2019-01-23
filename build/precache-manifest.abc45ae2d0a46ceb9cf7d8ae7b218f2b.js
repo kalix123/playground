623ab91526c90dce2708c1ff0bf0939923fc2d70
Kalix Zee <zeekalix@gmai.com>
2019-01-22 16:21:48 -0800

    first site commit

---
+++ build/precache-manifest.abc45ae2d0a46ceb9cf7d8ae7b218f2b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "9a0e57dfe2d3eaa24672",
+    "url": "./react/static/css/main.1bd6f0c0.chunk.css"
+  },
+  {
+    "revision": "9a0e57dfe2d3eaa24672",
+    "url": "./react/static/js/main.9a0e57df.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "./react/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "decc047a33a39582234d",
+    "url": "./react/static/js/runtime~main.decc047a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "./react/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "db7c5aae6a5a44c8e448de00bafd53aa",
+    "url": "./react/index.html"
+  }
+];
